6efaa434e960cc72ae8e61a7e6b5c15fd96ddf62
Cosmin C <concosminx@users.noreply.github.com>
2021-12-27 17:06:15 +0200

    modified backend server

--- src/App.js
+++ src/App.js
@@ -144,7 +144,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://morning-wildwood-30086.herokuapp.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -154,7 +154,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://morning-wildwood-30086.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
